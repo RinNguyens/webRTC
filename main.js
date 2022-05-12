f8fb6aabca96078b1ba6e06c12ef018e4dad4c04
rinnv <rin.nguyen@dirox.com>
2022-05-12 15:42:23 +0700

    1

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 (function () {
   "use strict";
-  var constraints = { audio: true, video: { facingMode: 'environment'} }
+  var constraints = {
+    audio: true,
+    video: {
+      width: { min: 1024, ideal: 1280, max: 1920 },
+      height: { min: 576, ideal: 850, max: 1080 },
+      facingMode: "environment",
+    },
+  };
   var video = document.querySelector("video");
-
   navigator.mediaDevices
     .getUserMedia(constraints)
     .then(function (mediaStream) {
       video.srcObject = mediaStream;
+      if (video.srcObject) {
+        videoIn.srcObject.getTracks().forEach((track) => {
+            track.stop()
+        });
+      }
       video.onloadedmetadata = function (e) {
         video.play();
       };
