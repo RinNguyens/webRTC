7987652779dc28323ba6d2d589e035532ef41476
rinnv <rin.nguyen@dirox.com>
2022-05-12 15:09:30 +0700

    .

--- main.js
+++ main.js
@@ -1,44 +1,17 @@
-(function() {
-    'use strict';
-    var video = document.querySelector('video')
-      , canvas;
+(function () {
+  "use strict";
+  var constraints = { audio: true, video:true, facingMode: { exact: 'environment'} };
+  var video = document.querySelector("video");
 
-    /**
-     *  generates a still frame image from the stream in the <video>
-     *  appends the image to the <body>
-     */
-
-    function takeSnapshot() {
-      var img = document.querySelector('img') || document.createElement('img');
-      var context;
-      var width = video.offsetWidth
-        , height = video.offsetHeight;
-
-
-      canvas = canvas || document.createElement('canvas');
-      canvas.width = width;
-      canvas.height = height;
-
-      context = canvas.getContext('2d');
-      context.drawImage(video, 0, 0, width, height);
-
-      img.src = canvas.toDataURL('image/png');
-      document.body.appendChild(img);
-    }
-
-    // use MediaDevices API
-    // docs: https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-    if (navigator.mediaDevices) {
-      // access the web cam
-      navigator.mediaDevices.getUserMedia({video: true})
-      // permission granted:
-        .then(function(stream) {
-          video.srcObject = stream;
-          video.addEventListener('click', takeSnapshot);
-        })
-        // permission denied:
-        .catch(function(error) {
-          document.body.textContent = 'Could not access the camera. Error: ' + error.name;
-        });
-    }
-  })();
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function (mediaStream) {
+      video.srcObject = mediaStream;
+      video.onloadedmetadata = function (e) {
+        video.play();
+      };
+    })
+    .catch(function (err) {
+      console.log(err.name + ": " + err.message);
+    }); // always check for errors at the end.
+})();
