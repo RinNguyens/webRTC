724ed3093e330e4245ab48464f82ec8e7088cda1
rinnv <rin.nguyen@dirox.com>
2022-05-13 11:52:30 +0700

    change

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
-(function () {
+(async function () {
   "use strict";
   var constraints = {
     audio: false,
-    video: true,
-    facingMode: { exact: "environment" }
+    video: { facingMode: 'enviroment' },
+    
   };
-  console.log(constraints, 'constraints');
+  console.log(constraints, "constraints");
   var video = document.querySelector("video");
   // Fix for iOS Safari from https://leemartin.dev/hello-webrtc-on-safari-11-e8bcb5335295
-  video.style.width = document.width + 'px';
-  video.style.height = document.height + 'px';
-  video.setAttribute('autoplay', '');
-  video.setAttribute('muted', '');
-  video.setAttribute('playsinline', '');
-  
+  video.style.width = document.width + "px";
+  video.style.height = document.height + "px";
+  video.setAttribute("autoplay", "");
+  video.setAttribute("muted", "");
+  video.setAttribute("playsinline", "");
 
-  navigator.mediaDevices
+  let result = { hasBack: false, hasFront: false, videoDevices: [] };
+
+  let videoinput = false;
+
+  await navigator.mediaDevices.enumerateDevices().then((devices) => {
+    if (devices.filter((device) => device.kind === "videoinput")) {
+      videoinput = true;
+    }
+  });
+
+  if (videoinput) {
+    navigator.mediaDevices
     .getUserMedia(constraints)
     .then(function (mediaStream) {
-        console.log(mediaStream, 'mediaStream');
       video.srcObject = mediaStream;
       video.onloadedmetadata = function (e) {
         video.play();
@@ -27,4 +36,42 @@
     .catch(function (err) {
       console.log(err.name + ": " + err.message);
     }); // always check for errors at the end.
+  }
+
+  async function hasFrontBack() {
+    let result = { hasBack: false, hasFront: false, videoDevices: [] };
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+      });
+      let devices = await navigator.mediaDevices.enumerateDevices();
+      const videoDevices = devices.filter((device) => {
+        if (device.kind === "videoinput") {
+          if (device.label && device.label.length > 0) {
+            if (device.label.toLowerCase().indexOf("back") >= 0) {
+              result.hasBack = true;
+            } else if (device.label.toLowerCase().indexOf("front") >= 0) {
+              result.hasFront = true;
+            } else {
+              /* some other device label ... desktop browser? */
+            }
+          }
+          return true;
+        }
+        return false;
+      });
+      result.videoDevices = videoDevices;
+      /* drop stream */
+      const tracks = stream.getTracks();
+      if (tracks) {
+        for (let t = 0; t < tracks.length; t++) tracks[t].stop();
+      }
+      return result;
+    } catch (ex) {
+      /* log and swallow exception, this is a probe only */
+      console.error(ex);
+      return result;
+    }
+  }
 })();
