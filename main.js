0e3ad59028af80cf4e1a0edf98635208493bfc83
rinnv <rin.nguyen@dirox.com>
2022-05-13 14:10:30 +0700

    change

--- main.js
+++ main.js
@@ -1,77 +1,55 @@
-(async function () {
-  "use strict";
-  var constraints = {
-    audio: false,
-    video: { facingMode: 'enviroment' },
-    
-  };
-  console.log(constraints, "constraints");
-  var video = document.querySelector("video");
-  // Fix for iOS Safari from https://leemartin.dev/hello-webrtc-on-safari-11-e8bcb5335295
-  video.style.width = document.width + "px";
-  video.style.height = document.height + "px";
-  video.setAttribute("autoplay", "");
-  video.setAttribute("muted", "");
-  video.setAttribute("playsinline", "");
+/*
+ *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+'use strict';
 
-  let result = { hasBack: false, hasFront: false, videoDevices: [] };
+// Put variables in global scope to make them available to the browser console.
+const constraints = window.constraints = {
+  audio: false,
+  video: true
+};
 
-  let videoinput = false;
+function handleSuccess(stream) {
+  const video = document.querySelector('video');
+  const videoTracks = stream.getVideoTracks();
+  console.log('Got stream with constraints:', constraints);
+  console.log(`Using video device: ${videoTracks[0].label}`);
+  window.stream = stream; // make variable available to browser console
+  video.srcObject = stream;
+}
 
-  await navigator.mediaDevices.enumerateDevices().then((devices) => {
-    if (devices.filter((device) => device.kind === "videoinput")) {
-      videoinput = true;
-    }
-  });
-
-  if (videoinput) {
-    navigator.mediaDevices
-    .getUserMedia(constraints)
-    .then(function (mediaStream) {
-      video.srcObject = mediaStream;
-      video.onloadedmetadata = function (e) {
-        video.play();
-      };
-    })
-    .catch(function (err) {
-      console.log(err.name + ": " + err.message);
-    }); // always check for errors at the end.
+function handleError(error) {
+  if (error.name === 'OverconstrainedError') {
+    const v = constraints.video;
+    errorMsg(`The resolution ${v.width.exact}x${v.height.exact} px is not supported by your device.`);
+  } else if (error.name === 'NotAllowedError') {
+    errorMsg('Permissions have not been granted to use your camera and ' +
+      'microphone, you need to allow the page access to your devices in ' +
+      'order for the demo to work.');
   }
+  errorMsg(`getUserMedia error: ${error.name}`, error);
+}
 
-  async function hasFrontBack() {
-    let result = { hasBack: false, hasFront: false, videoDevices: [] };
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio: false,
-      });
-      let devices = await navigator.mediaDevices.enumerateDevices();
-      const videoDevices = devices.filter((device) => {
-        if (device.kind === "videoinput") {
-          if (device.label && device.label.length > 0) {
-            if (device.label.toLowerCase().indexOf("back") >= 0) {
-              result.hasBack = true;
-            } else if (device.label.toLowerCase().indexOf("front") >= 0) {
-              result.hasFront = true;
-            } else {
-              /* some other device label ... desktop browser? */
-            }
-          }
-          return true;
-        }
-        return false;
-      });
-      result.videoDevices = videoDevices;
-      /* drop stream */
-      const tracks = stream.getTracks();
-      if (tracks) {
-        for (let t = 0; t < tracks.length; t++) tracks[t].stop();
-      }
-      return result;
-    } catch (ex) {
-      /* log and swallow exception, this is a probe only */
-      console.error(ex);
-      return result;
-    }
+function errorMsg(msg, error) {
+  const errorElement = document.querySelector('#errorMsg');
+  errorElement.innerHTML += `<p>${msg}</p>`;
+  if (typeof error !== 'undefined') {
+    console.error(error);
   }
-})();
+}
+
+async function init(e) {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+    e.target.disabled = true;
+  } catch (e) {
+    handleError(e);
+  }
+}
+
+document.querySelector('#showVideo').addEventListener('click', e => init(e));
