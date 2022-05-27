71dd75185e6e90de08ebe45520c005730cee5f3e
RinNguyen <rin.nguyen@dirox.com>
2022-05-27 11:52:47 +0700

    switch camera v5

--- main.js
+++ main.js
@@ -241,9 +241,9 @@ async function onShutter() {
   if (getWeight.height > startFrame.height) {
     frameResult.style.top = '50%';
     frameResult.style.left = '30%';
-  } else if (getWeight.height < 250) {
-    frameResult.style.top = '53%';
-    frameResult.style.left = '33.5%';
+  } else if (getWeight.height <= 250) {
+    frameResult.style.top = '55%';
+    frameResult.style.left = '34%';
   }
 
 
