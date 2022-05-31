c9c3ac4c7e9bd5b2de4a9ea428949d1da1b3df0d
RinNguyen <rin.nguyen@dirox.com>
2022-05-31 11:43:12 +0700

    handle zoom 0.7

--- scale.js
+++ scale.js
@@ -52,8 +52,8 @@ const distanceScale = (event) => {
 
 imageElement.addEventListener("touchstart", (event) => {
   const el = document.querySelector("#frame");
-    el.style.transform = `scale(${0.8})`;
-    console.log(0.8);
+    el.style.transform = `scale(${0.7})`;
+    console.log(0.7);
   if (event.touches.length === 2) {
     event.preventDefault(); // Prevent page scroll
 
